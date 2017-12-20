266c1fb108da3f742925c6ab75e7d254d4536b16
robinchoii <robinchoii@gmail.com>
2017-12-20 00:06:37 -0800

    export menu to separate vue file

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,15 @@
 
 	<div id="app" class='container'>
 
-		<nav-bar></nav-bar>
-
-		<lunch-menu ></lunch-menu>
-
-		<appetizers-menu> </appetizers-menu>
-
-		<soft-tofu-soup-menu></soft-tofu-soup-menu>
-
-		<bibimbob-menu></bibimbob-menu>
-
-		<bbq-menu></bbq-menu>
-
-		<fried-chicken-menu></fried-chicken-menu>
-
-		<grilled-fish-menu></grilled-fish-menu>
-
-		<sauteed-dishes-menu></sauteed-dishes-menu>
+		<full-menu></full-menu>
 
 	</div>
 
 </template>
 
 <script>
-		import NavBar from './components/NavBar.vue'
-		import LunchMenu from './components/LunchMenu.vue'
-		import AppetizersMenu from './components/AppetizersMenu.vue'
-		import SoftTofuSoupMenu from './components/SoftTofuSoupMenu.vue'
-		import BibimbobMenu from './components/BiBimBobMenu.vue'
-		import BbqMenu from './components/BBQMenu.vue'
-		import FriedChickenMenu from './components/FriedChickenMenu.vue'
-		import GrilledFishMenu from './components/GrilledFishMenu.vue'
-		import SauteedDishesMenu from './components/SauteedDishesMenu.vue'
+
+		import FullMenu from './components/FullMenu.vue'
 
 		export default {
 
@@ -41,7 +18,7 @@
 
 				components: {
 
-						NavBar, LunchMenu, AppetizersMenu, SoftTofuSoupMenu, BibimbobMenu, BbqMenu, FriedChickenMenu, GrilledFishMenu, SauteedDishesMenu
+					FullMenu
 
 				},
 
@@ -60,26 +37,5 @@
 </script>
 
 <style>
-	#app {
 
-		/*border: solid 1px black;*/
-
-	}
-
-	.item-description {
-		line-height: 1;
-		font-size: .75em;
-	}
-
-	.item-price {
-		color: black;
-		font-size:.8em;
-		font-weight: 600;
-	}
-
-	.item-title {
-		font-weight: 600;
-		line-height: 1;
-		margin-bottom: 3px
-	}
 </style>
---
+++ src/components/FullMenu.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+
+        <nav-bar></nav-bar>
+
+        <lunch-menu ></lunch-menu>
+
+        <appetizers-menu> </appetizers-menu>
+
+        <soft-tofu-soup-menu></soft-tofu-soup-menu>
+
+        <bibimbob-menu></bibimbob-menu>
+
+        <bbq-menu></bbq-menu>
+
+        <fried-chicken-menu></fried-chicken-menu>
+
+        <grilled-fish-menu></grilled-fish-menu>
+
+        <sauteed-dishes-menu></sauteed-dishes-menu>
+
+    </div>
+</template>
+
+<script type="text/javascript">
+    import NavBar from './NavBar.vue'
+    import LunchMenu from './LunchMenu.vue'
+    import AppetizersMenu from './AppetizersMenu.vue'
+    import SoftTofuSoupMenu from './SoftTofuSoupMenu.vue'
+    import BibimbobMenu from './BiBimBobMenu.vue'
+    import BbqMenu from './BBQMenu.vue'
+    import FriedChickenMenu from './FriedChickenMenu.vue'
+    import GrilledFishMenu from './GrilledFishMenu.vue'
+    import SauteedDishesMenu from './SauteedDishesMenu.vue'
+
+    export default {
+
+        components: {
+
+                NavBar, LunchMenu, AppetizersMenu, SoftTofuSoupMenu, BibimbobMenu, BbqMenu, FriedChickenMenu, GrilledFishMenu, SauteedDishesMenu
+
+        },
+    }
+</script>
+
+<style type="text/css">
+
+    .item-description {
+        line-height: 1;
+        font-size: .75em;
+    }
+
+    .item-price {
+        color: black;
+        font-size:.8em;
+        font-weight: 600;
+    }
+
+    .item-title {
+        font-weight: 600;
+        line-height: 1;
+        margin-bottom: 3px
+    }
+</style>
