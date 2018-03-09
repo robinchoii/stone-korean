a5194e45e1d94300b5e2cc3769bc21a3bab1f29f
robinchoii <robinchoii@gmail.com>
2018-03-08 17:52:17 -0800

    drop marker on google map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,35 +1,37 @@
 <template>
 
+
     <div class='google-map' id="stone-map"></div>
+
 
 </template>
 
 <script type="text/javascript">
 
-
     export default {
         name: 'google-map',
+
         props: ['name'],
-        data: function() {
+
+        data: () => {
             return {
-                mapName: this.name+ "-map",
+
             }
         },
 
-        mounted: () => {
+        mounted: function() {
             const element = document.getElementById('stone-map')
             const options = {
                 zoom: 15,
                 center: new google.maps.LatLng(47.671285,-122.114915)
             }
-
             const map = new google.maps.Map(element, options);
 
-            const coordinates = new google.maps.LatLng(47.671285,-122.114915)
+            const position = new google.maps.LatLng(47.671285,-122.114915)
 
             const marker = new google.maps.Marker({
-                coordinates
-
+                position: position,
+                title:'Stone Korean Restaurant'
             })
 
             marker.setMap(map)
@@ -43,8 +45,8 @@
 
 <style type="text/css">
     .google-map {
-        width: 800px;
-        height: 600px;
+        width: 600px;
+        height: 400px;
         margin: 0 auto;
         background: gray;
 
