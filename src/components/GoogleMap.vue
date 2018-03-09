b799f6990a953c51575e0be1d85f4b9c23b52e70
robinchoii <robinchoii@gmail.com>
2018-03-08 17:27:52 -0800

    google map component working

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <div class='google-map' id="stone-map"></div>
+
+</template>
+
+<script type="text/javascript">
+
+
+    export default {
+        name: 'google-map',
+        props: ['name'],
+        data: function() {
+            return {
+                mapName: this.name+ "-map",
+            }
+        },
+
+        mounted: () => {
+            const element = document.getElementById('stone-map')
+            const options = {
+                zoom: 15,
+                center: new google.maps.LatLng(47.671285,-122.114915)
+            }
+
+            const map = new google.maps.Map(element, options);
+
+            const coordinates = new google.maps.LatLng(47.671285,-122.114915)
+
+            const marker = new google.maps.Marker({
+                coordinates
+
+            })
+
+            marker.setMap(map)
+
+        }
+
+    }
+
+
+</script>
+
+<style type="text/css">
+    .google-map {
+        width: 800px;
+        height: 600px;
+        margin: 0 auto;
+        background: gray;
+
+    }
+</style>
