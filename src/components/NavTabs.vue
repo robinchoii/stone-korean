325c2e0ff3117e58d719d920bda2fb2ba9dcceac
Robin Choi <robinchoii@gmail.com>
2018-03-19 16:45:25 -0700

    redo nav tabs

--- src/components/NavTabs.vue
+++ src/components/NavTabs.vue
@@ -6,9 +6,21 @@
 
             <ul>
 
-                <li v-for='tab in tabs' :class="{'is-active' : tab.isActive}">
+                <li><a href="/" title="">Home</a></li>
 
-                    <a :href='tab.href' @click='selectTab(tab)'>{{tab.name}} {{tab.title}}</a>
+                <li><a href="#contact" title="">Contact</a></li>
+
+                <li class='menu-list' @click='toggleMenu'>
+                    <span id='menu'>Menu</span>
+                    <ul class="" v-for='tab in tabs' v-show='isMenuOpen'>
+
+                        <li  :class="{'is-active' : tab.isActive}">
+
+                            <a :href='tab.href' @click='selectTab(tab)'>{{tab.name}} {{tab.title}}</a>
+
+                        </li>
+
+                    </ul>
 
                 </li>
 
@@ -34,7 +46,8 @@ export default {
 
   data () {
     return {
-        tabs: []
+        tabs: [],
+        isMenuOpen: false
     }
   },
 
@@ -49,6 +62,14 @@ export default {
         this.tabs.forEach( tab => {
             tab.isActive = (tab.name == selectedTab.name);
         })
+    },
+    toggleMenu(tab) {
+        console.log(tab)
+        if (this.isMenuOpen === false) {
+            this.isMenuOpen = true
+        } else {
+            this.isMenuOpen = false
+        }
     }
   }
 
@@ -59,15 +80,21 @@ export default {
 <style lang="css" scoped>
     .nav-tabs {
         display: grid;
-        grid-template-columns: auto 1fr;
+        grid-template-columns: 1fr;
         grid-gap: 25px;
         grid-template-areas:
-            "tabs tabs-detail"
+            "tabs"
+            "tabs-detail"
     }
     .tabs  {
         grid-area: tabs;
         letter-spacing: 0px;
+
      }
+        .tabs ul {
+            margin: 0;
+            padding: 0;
+        }
 
      .tabs-detail{
         grid-area: tabs-details;
@@ -80,6 +107,9 @@ export default {
         line-height: 2.5;
         text-transform: uppercase;
         color: #c8a5aa;
+        margin:10px;
+        padding: 0px;
+        text-align: center;
 
     }
     .tabs ul li a {
@@ -102,18 +132,33 @@ export default {
         max-width: 1200px;
     }
 
-    @media only screen and (max-width: 600px) {
-        .nav-tabs {
-            grid-template-columns: 1fr;
-            grid-template-areas:
-                "tabs"
-                "tabs-detail"
+
+    #menu {
+        font-weight: 700;
+        grid-column: 1;
+    }
+
+    .menu-list ul {
+        grid-row: 2;
+        /*grid-column: 1;*/
+    }
+
+
+
+    @media only screen and (min-width: 800px) {
+        .menu-list {
+            display:grid;
+            grid-template-columns: repeat(auto-fill, minmax(auto,1fr));
+            grid-column-gap: 5px;
         }
 
         .tabs ul {
             display: grid;
-            /*grid-row-gap: 50px;*/
-            grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+            grid-auto-flow: column;
+            grid-gap: 20px;
+            grid-template-columns: repeat(auto-fit, minmax(auto, 1fr));
+            justify-content: left;
+
         }
     }
 </style>
---
+++ src/components/Reviews.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <div>
+            Reviews
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+
+      name: 'Reviews',
+
+      data () {
+        return {
+
+        }
+      }
+    }
+</script>
+
+<style lang="css" scoped>
+</style>
