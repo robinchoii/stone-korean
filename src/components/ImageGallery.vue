9441eee664c97822ed6e24d42f5c0704e19c8f54
Robin Choi <robinchoii@gmail.com>
2018-05-17 17:46:18 -0700

    upload new pictures

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -18,87 +18,76 @@
       data () {
         return {
             images: [
-            {
-                id: 23,
-                path: '/assets/stone23.jpg'
-            },
-            {
-                id: 11,
-                path: '/assets/stone11.jpg'
-            },
-            {
-                id: 12,
-                path: '/assets/stone12.jpg'
-            },
-            {
-                id: 6,
-                path: '/assets/stone6.jpg'
-            },
+            // {
+            //     id: 1,
+            //     path: '/src/assets/stone-pic1.jpg',
+            // },
             {
                 id: 16,
-                path: '/assets/stone16.jpg'
-            },
-            {
-                id: 17,
-                path: '/assets/stone17.jpg'
-            },
-            {
-                id: 7,
-                path: '/assets/stone7.jpg'
-            },
-            {
-                id: 2,
-                path: '/assets/stone2.jpg'
-            },
-            {
-                id: 13,
-                path: '/assets/stone13.jpg'
-            },
-            {
-                id: 15,
-                path: '/assets/stone15.jpg'
-            },
-            {
-                id: 10,
-                path: '/assets/stone10.jpg'
-            },
-            {
-                id: 3,
-                path: '/assets/stone3.jpg'
-            },
-            {
-                id: 20,
-                path: '/assets/stone20.jpg'
+                path: '/src/assets/stone-pic16.jpg',
             },
             {
                 id: 14,
-                path: '/assets/stone14.jpg'
+                path: '/src/assets/stone-pic14.jpg',
             },
             {
-                id: 4,
-                path: '/assets/stone4.jpg'
+                id: 15,
+                path: '/src/assets/stone-pic15.jpg',
             },
             {
-                id: 9,
-                path: '/assets/stone9.jpg'
-            },
-
-            {
-                id: 1,
-                path: '/assets/stone1.jpg'
+                id: 11,
+                path: '/src/assets/stone-pic11.jpg',
             },
             {
-                id: 18,
-                path: '/assets/stone18.jpg'
+                id: 3,
+                path: '/src/assets/stone-pic3.jpg',
             },
             {
                 id: 5,
-                path: '/assets/stone5.jpg'
+                path: '/src/assets/stone-pic5.jpg',
             },
             {
-                id: 19,
-                path: '/assets/stone19.jpg'
-            }]
+                id: 6,
+                path: '/src/assets/stone-pic6.jpg',
+            },
+            {
+                id: 17,
+                path: '/src/assets/stone-pic17.jpg',
+            },
+            {
+                id: 2,
+                path: '/src/assets/stone-pic2.jpg',
+            },
+            // {
+            //     id: 7,
+            //     path: '/src/assets/stone-pic7.jpg',
+            // },
+            // {
+            //     id: 8,
+            //     path: '/src/assets/stone-pic8.jpg',
+            // },
+            {
+                id: 12,
+                path: '/src/assets/stone-pic12.jpg',
+            },
+            {
+                id: 9,
+                path: '/src/assets/stone-pic9.jpg',
+            },
+            {
+                id: 13,
+                path: '/src/assets/stone-pic13.jpg',
+            },
+            {
+                id: 10,
+                path: '/src/assets/stone-pic10.jpg',
+            },
+            {
+                id: 4,
+                path: '/src/assets/stone-pic4.jpg',
+            },
+
+            ]
         }
       }
     }
@@ -107,7 +96,7 @@
 <style lang="css" scoped>
     .gallery {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
         justify-items: center;
         grid-row-gap: 20px;
 
