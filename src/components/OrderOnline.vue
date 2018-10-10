ba41f6bc293cb9d5faf275607fbb8a7051aeec1b
Robin Choi <robinchoii@gmail.com>
2018-10-09 22:14:46 -0700

    add links to redmond take out and delivery

--- src/components/OrderOnline.vue
+++ src/components/OrderOnline.vue
@@ -5,10 +5,12 @@
             <h2>Redmond</h2>
             <h2>Seattle</h2>
             <div class="order-card">
-              <div class="order-wrapper">
-                <h3>Pick&nbsp;Up</h3>
-                <i class="fa fa-4x fa-shopping-bag" aria-hidden="true"></i>
-              </div>
+                <a href="https://www.trycaviar.com/t/seattle-eastside/stone-korean-8604?groups=f_online__d_dwidget-4-button-1__p_marketing__c_merchant-links&how=pickup&utm_medium=localmarketing&utm_source=widget&utm_campaign=stone-korean-8604&utm_term=seattle-eastside&utm_content=stone-korean-8604&cvosrc=localmarketing.widget.stone-korean-8604&cvo_campaign=stone-korean-8604&cav_source=stone-korean-8604&cav_medium=widget">
+                    <div class="order-wrapper">
+                        <h3>Pick&nbsp;Up</h3>
+                        <i class="fa fa-4x fa-shopping-bag" aria-hidden="true"></i>
+                    </div>
+                </a>
             </div>
 
             <div class="order-card">
@@ -19,10 +21,12 @@
             </div>
 
             <div class="order-card">
-              <div class="order-wrapper">
-                <h3>Delivery</h3>
-                <i class="fa fa-4x fa-car" aria-hidden="true"></i>
-              </div>
+                <a href="https://www.trycaviar.com/t/seattle-eastside/stone-korean-8604?groups=f_online__d_dwidget-4-button-1__p_marketing__c_merchant-links&how=delivery&utm_medium=localmarketing&utm_source=widget&utm_campaign=stone-korean-8604&utm_term=seattle-eastside&utm_content=stone-korean-8604&cvosrc=localmarketing.widget.stone-korean-8604&cvo_campaign=stone-korean-8604&cav_source=stone-korean-8604&cav_medium=widget">
+                    <div class="order-wrapper">
+                        <h3>Delivery</h3>
+                        <i class="fa fa-4x fa-car" aria-hidden="true"></i>
+                    </div>
+                </a>
             </div>
 
             <div class="order-card">
@@ -49,16 +53,20 @@
     }
 
     .order-card {
-    /* Add shadows to create the "card" effect */
-        background-color: #00000073;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    padding: 25px;
-}
+        /* Add shadows to create the "card" effect */
+            background-color: #00000073;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        padding: 25px;
+    }
 
-/* On mouse-over, add a deeper shadow */
-.order-card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
+    .order-card a {
+        text-decoration: none;
+        color: #e2e2e2;
+    }
+    /* On mouse-over, add a deeper shadow */
+    .order-card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
 
 </style>
