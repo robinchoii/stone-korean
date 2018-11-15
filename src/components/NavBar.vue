10e5139e5000fbfedf0f7e6ab96004f4d45c909d
Robin Choi <robinchoii@gmail.com>
2018-11-14 18:45:45 -0800

    add new logo imagesr

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -19,13 +19,14 @@
 <style type="text/css">
      .logo-container {
         margin: 0 auto;
-        width: 293px;
+        width: 325px;
      }
      .logo-image {
-        background-image: url('/src/assets/stone-logo.png');
+        background-image: url('/src/assets/stone-logo-2.png');
         background-repeat: no-repeat;
+        background-size: 325px 78px;
         height: 78px;
-        width: 293px;
+        width: 325px;
         margin:0;
      }
      .logo-image:hover {
