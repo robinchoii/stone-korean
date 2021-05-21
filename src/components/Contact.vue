92da7e250287bc7a9b2081a7a62cf8078176e84a
ROBIN CHOI <robinchoi@robins-macbook.lan>
2021-05-20 20:06:48 -0700

    add bothell to contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -18,11 +18,13 @@
                     </div>
                 </div>
                 <div class="info-card">
-                    <div class='address'>
-                        <div><i class="fa fa-2x fa-location-arrow"></i></div>
-                        <a href='https://www.google.com/maps/place/Stone+Korean+Restaurant/@47.6712848,-122.1171033,17z/data=!3m1!4b1!4m5!3m4!1s0x549072b0849aab65:0x323ab2d85984f5b!8m2!3d47.6712848!4d-122.1149146'>16857 Redmond Way<br>
-                        Redmond, WA 98052</a>
-                    </div>
+                    <a href='https://goo.gl/maps/iHff5CKSHfYoGWYR7'>
+                        <div class='address'>
+                            <div><i class="fa fa-2x fa-location-arrow"></i></div>
+                            <div>16857 Redmond Way</div><br>
+                            <div>Redmond, WA 98052</div>
+                        </div>
+                    </a>
                 </div>
                 <div class="info-card">
                     <div class='phone'>
@@ -42,15 +44,44 @@
                     </div>
                 </div>
                 <div class="info-card">
-                    <div class='address'>
-                        <div><i class="fa fa-2x fa-location-arrow"></i></div>
-                        <a href='https://www.google.com/maps/place/Stone+Korean+Restaurant/@47.6272525,-122.3443252,17z/data=!3m1!4b1!4m5!3m4!1s0x549015f23eacc7df:0x6f755914399c427d!8m2!3d47.6272525!4d-122.3421365'>900 Dexter Ave N<br>Seattle, WA 98109</a>
-                    </div>
+                    <a href='https://goo.gl/maps/Ga7t8o45xo68WuCf8'>
+                        <div class='address'>
+                            <div><i class="fa fa-2x fa-location-arrow"></i></div>
+                            <div>900 Dexter Ave N</div><br>
+                            <div>Seattle, WA 98109</div>
+                        </div>
+                    </a>
                 </div>
                 <div class="info-card">
                     <div class='phone'>
                         <div><i class="fa fa-2x fa-phone"></i></div>
                         <a href='tel:+12067172864'>206-717-2864</a>
+                    </div>
+                </div>
+            </div>
+            <div class='info-bothell' style='text-align:center;'>
+                <h2><span class='new-location'>New!</span>Bothell</h2>
+                <div class="info-card">
+                    <div class='hours'>
+                        <div><i class="fa fa-2x fa-clock-o"></i></div>
+                        <div><b>Monday - Thursday:</b><br> 11AM - 8:00PM</div>
+                        <div><b>Friday - Saturday:</b><br> 11AM - 8:30PM </div>
+                        <div><b>Sunday:</b><br> 11AM - 8:00PM</div>
+                    </div>
+                </div>
+                <div class="info-card">
+                    <a href='https://goo.gl/maps/ud8pPSQbjz9NYiwD7'>
+                        <div class='address'>
+                            <div><i class="fa fa-2x fa-location-arrow"></i></div>
+                            <div>24032 Bothell Everett Hwy Suite #400</div>
+                            <div>Bothell, WA 98021</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="info-card">
+                    <div class='phone'>
+                        <div><i class="fa fa-2x fa-phone"></i></div>
+                        <a href='tel:+12067172864'>425-408-1462</a>
                     </div>
                 </div>
             </div>
@@ -88,6 +119,14 @@
 
     .info-card div {
         margin: 3px;
+    }
+
+    .new-location {
+        margin-right: 5px;
+        color: #caae00;
+        font-size: 14px;
+        position: relative;
+        top: 0;
     }
 
     @media only screen and (max-width: 735px) {
